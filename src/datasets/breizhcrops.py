f84c7acb6bc858ee058cae53f9dc72b506f05684
Marc Rußwurm <marc.russwurm@tum.de>
2020-02-02 02:25:06 +0100

    added TempCNN and MS-resnet models

--- src/datasets/breizhcrops.py
+++ src/datasets/breizhcrops.py
@@ -270,6 +270,18 @@ def update_progress(progress):
     text = "\rLoaded: [{0}] {1:.2f}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
     sys.stdout.write(text)
     sys.stdout.flush()
+    
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_url(url, output_path):
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
 
 if __name__=="__main__":
     root = "/home/marc/projects/BreizhCrops/data"

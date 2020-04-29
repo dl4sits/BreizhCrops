51f3201267523b765fb016003661a9e4d1cc41ba
Marc Rußwurm <marc.russwurm@tum.de>
2020-04-29 15:09:18 +0200

    fixed test dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-try:
+#try:
     # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:
+from pip._internal.req import parse_requirements
+#except ImportError:
     # for pip <= 9.0.3
-    from pip.req import parse_requirements
+#    from pip.req import parse_requirements
 
 def load_requirements(fname):
     reqs = parse_requirements(fname, session="test")

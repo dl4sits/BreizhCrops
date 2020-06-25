971c2e4506170fa30f39235b19839049ba77f4c9
Marc Rußwurm <marc.russwurm@tum.de>
2020-06-25 14:13:21 +0200

    added doa to belle-ile dataset

--- processing/select_belle-ile_from_frh04.py
+++
@@ -1,6 +0,0 @@
-from breizhcrops import BreizhCrops
-
-frh04_l2a = BreizhCrops("frh04", level="L2A", root="/tmp/data")
-belle_ile = BreizhCrops("belle-ile", root="/tmp/data", level="L1C")
-
-belleile_l2a = BreizhCrops("belle-ile", level="L2A")
